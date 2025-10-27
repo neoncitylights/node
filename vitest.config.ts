363185b5b53ef2a5ea6117bf816d561435cceeba
Samantha <contact@samanthanguyen.me>
2025-10-27 12:52:15 -0500

    deps: upgrade vitest to v4.0.4, configure directories (#457)

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,12 +5,13 @@ export default defineConfig({
 		coverage: {
 			include: ['packages/**/src'],
 			provider: 'v8',
+			reportsDirectory: 'dist/vitest/coverage',
 		},
 		projects: ['packages/*'],
 		outputFile: {
-			html: 'dist/vitest/index.html',
-			json: 'dist/vitest/index.json',
-			clover: 'dist/vitest/index.xml',
+			html: 'dist/vitest/report/index.html',
+			json: 'dist/vitest/report/index.json',
+			clover: 'dist/vitest/report/index.xml',
 		}
 	},
 })
