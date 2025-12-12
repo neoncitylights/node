e0a26ed70b0cd982f012ea0fb7bf82c2b9c429bc
Samantha Nguyen <contact@samanthanguyen.me>
2025-12-11 21:48:53 -0600

    Update tsdown.config.ts

--- packages/pkg1/tsdown.config.ts
+++ packages/pkg1/tsdown.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
 	entry: 'src/index.ts',
 	dts: true,
-	publint: true,
+	publint: {
+		strict: false,
+	},
 	minify: true,
 	nodeProtocol: true,
 })
